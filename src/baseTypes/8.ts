c25837e04f2bd22df4ad06440ddfaff1c96e8948
NataliaPot <137684728+NataliaPot@users.noreply.github.com>
2024-02-17 20:03:36 +0100

    first commit

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -3,6 +3,8 @@
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
 
-const myGender;
+type Gender = "female" | "male"
+
+const myGender: Gender = 'male'
 
 export {};
